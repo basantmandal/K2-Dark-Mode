47ce354d0bbe9d822c0c0e348e9b2a69a3500480
Basant (Kitto) <hi@techbasant.in>
2020-11-17 21:14:27 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# K2DarkMode
+# K2 DarkMode - A JavaScript Dark Mode Plugin
+
+## Features
+    * Easy & Lightweight (~6kb)
+    * Time can be added for Automatic Applying Dark Mode
+    * Very Easy to Customize as per your needs - Only CSS needs to be updated.
+
+**How can you use for test or learning purpose ? You can go through the index.html or else read below**
+
+
+Please note:- **This plugin is under development.**
---
+++ k2darkmode.js
@@ -0,0 +1,72 @@
+/*
+ * k2darkmode.js 0.0.1 - 17 - November - 2020
+ * Copyright (c) 2020 Basant Mandal, https://wwww.techbasant.in
+ * k2darkmode.js is open sourced under the MIT license.
+ * https://github.com/basantmandal/K2DarkMode
+ */
+var settings = {
+    debug: false,
+    version: "0.0.1"
+};
+console.clear();
+console.log("Dark JS Active\nVersion:- " + settings.version);
+var defaults = {
+    light: "assets/css/light.css",
+    dark: "assets/css/dark.css",
+    startAt: '13:00',
+    endAt: '06:00',
+};
+/*
+ * PURPOSE : ADD DARK CLASS ON THE HEAD SECTION
+ *  PARAMS :  fileName (Can be with Path like assets/css/darkmode.css)
+ * RETURNS :  True/False
+ */
+function addCss(fileName) {
+    var head = document.head;
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = fileName;
+    link.id = "darkMode_css"
+    head.appendChild(link);
+}
+/*
+ * PURPOSE : REMOVES DARK MODE
+ *  PARAMS :
+ * RETURNS :  Nothing
+ */
+function removeDarkMode() {
+    const stylesheet = document.getElementById("darkMode_css");
+    stylesheet.parentNode.removeChild(stylesheet);
+}
+/*
+ * PURPOSE : Converts to Unix Time Stamp
+ *  PARAMS :  Valid Time like 13:00 (24Hrs Format)
+ * RETURNS :  Unix Time Stamp
+ */
+var normalizeTime = function (time) {
+    let date = new Date();
+    let normalized = time.split(':');
+    return date.setHours(normalized[0], normalized[1], 0);
+}
+/*
+ * PURPOSE : Main Function which does the job
+ *  PARAMS :  Valid Time like 13:00 (24Hrs Format)
+ * RETURNS :  Unix Time Stamp
+ */
+var darkMode = (options = defaults, debug = settings.debug) => {
+    let dateObject = new Date();
+    let startTime = normalizeTime(options.startAt);
+    let endTime = normalizeTime(options.endAt);
+    let currentTime = dateObject.getTime();
+    let status = (endTime < currentTime && currentTime > startTime) || (startTime > currentTime && currentTime < endTime) ? addCss(options.dark) : addCss(options.light);
+    if (debug) {
+        console.clear();
+        console.log("Dark JS Active\nVersion:- " + settings.version + " Debug Mode Enabled\n");
+        console.log("Start Time :- " + options.startAt);
+        console.log("End Time :- " + options.endAt);
+        console.log("Current Time :- " + dateObject.toDateString());
+        let mode = (endTime < currentTime && currentTime > startTime) || (startTime > currentTime && currentTime < endTime) ? "Dark Mode" : "Normal Mode";
+        console.log("Mode :- " + mode);
+    }
+}
